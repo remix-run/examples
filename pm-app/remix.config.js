068194a2516b82ec3ac7385279c1783a0897a37f
Chance Strickland <hi@chance.dev>
2022-09-15 12:03:50 -0700

    init

---
+++ pm-app/remix.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+};
+
+module.exports = {
+  // appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "server/build",
+};
