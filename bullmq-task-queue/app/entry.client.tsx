1f50889fb527128ae72b517e476d8cf59121b8b1
Janos Hardi <janos.hardi@gmail.com>
2022-09-27 12:38:07 +0200

    revert React and util module versions

--- bullmq-task-queue/app/entry.client.tsx
+++ bullmq-task-queue/app/entry.client.tsx
@@ -1,21 +1,3 @@
-import { startTransition, StrictMode } from "react";
 import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
-
-const hydrate = () => {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
-};
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
-}
+import { hydrate } from "react-dom";
+hydrate(<RemixBrowser />, document);
