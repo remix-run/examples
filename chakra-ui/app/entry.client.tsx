c254188be05826d7e22631fdbd94a3804a0d7821
Jacob Ebey <jacob.ebey@live.com>
2022-10-19 13:18:07 -0700

    chore: clean up chakra example

--- chakra-ui/app/entry.client.tsx
+++ chakra-ui/app/entry.client.tsx
@@ -1,4 +1,28 @@
 import { RemixBrowser } from "@remix-run/react";
-import { hydrate } from "react-dom";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import createEmotionCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 
-hydrate(<RemixBrowser />, document);
+function hydrate() {
+  const emotionCache = createEmotionCache({ key: "css" });
+
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <CacheProvider value={emotionCache}>
+          <RemixBrowser />
+        </CacheProvider>
+      </StrictMode>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
