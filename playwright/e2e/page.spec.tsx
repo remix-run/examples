c1723bc353f24b428d2b845c871b2d0f2321a76b
irvile <irvile.rodrigues@gmail.com>
2022-10-21 15:14:24 -0300

    feat: add playwright example

---
+++ playwright/e2e/page.spec.tsx
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("should navigate to the login page", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator("h1")).toContainText(
+    "Welcome to Remix + Playwright example"
+  );
+  await page.click("text=Login");
+
+  await expect(page).toHaveURL("/login");
+  await expect(page.locator("h1")).toContainText("Login Page");
+});
