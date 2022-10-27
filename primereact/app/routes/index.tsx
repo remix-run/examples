400345043b0be9d9a3fb39e047419aa60744309d
KCampos-GandG <kendall14campos@hotmail.com>
2022-10-27 16:58:24 -0600

    Update

--- primereact/app/routes/index.tsx
+++ primereact/app/routes/index.tsx
@@ -1,8 +1,8 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { Button } from "primereact/button";
 
 export default function Index() {
-    const [count,setCount] = useState(0);
+    const [count, setCount] = useState(0);
     return (
         <div className="text-center">
             <Button label="Click" icon="pi pi-plus" onClick={e => setCount(count + 1)}></Button>
