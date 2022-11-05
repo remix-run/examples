a0fcbf39be8d694982151273a65c4f20d5721f03
David Michael II <knowdavidmichael@gmail.com>
2022-11-05 18:00:01 +0100

    build: add eslint config for baseui

--- with-baseweb/.eslintrc.js
+++ with-baseweb/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  plugins: ["baseui"],
+  rules: {
+    'baseui/deprecated-theme-api': "warn",
+    'baseui/deprecated-component-api': "warn",
+    'baseui/no-deep-imports': "warn",
+  }
 };
