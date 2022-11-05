8b73cefabfd174321bb8a1cb061bb62c41f82415
David Michael II <knowdavidmichael@gmail.com>
2022-11-05 23:41:54 +0100

    feat: add styletron and base provider to root

--- with-baseweb/app/root.tsx
+++ with-baseweb/app/root.tsx
@@ -8,11 +8,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { LightTheme, BaseProvider } from "baseui";
+import { DebugEngine, Provider as StyletronProvider } from "styletron-react";
+import { styletron } from "./styletron";
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
 
 export default function App() {
   return (
@@ -22,7 +28,11 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
+          <BaseProvider theme={LightTheme}>
+            <Outlet />
+          </BaseProvider>
+        </StyletronProvider>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
