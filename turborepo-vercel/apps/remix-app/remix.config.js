3eb0a02a259735523bd6375b6964d371bfff9ce2
Logan McAnsh <logan@mcan.sh>
2022-12-05 21:42:20 -0500

    chore(turborepo-vercel): add `watchPaths` config (#94)

--- turborepo-vercel/apps/remix-app/remix.config.js
+++ turborepo-vercel/apps/remix-app/remix.config.js
@@ -1,3 +1,15 @@
+const path = require("node:path");
+
+const glob = require("glob");
+
+const packages = glob
+  .sync("packages/**/package.json", {
+    cwd: path.join(__dirname, "..", ".."),
+    ignore: ["**/node_modules/**"],
+    absolute: true,
+  })
+  .map((pkg) => path.dirname(pkg));
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
@@ -11,4 +23,5 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   serverDependenciesToBundle: [/.*/],
+  watchPaths: packages,
 };
