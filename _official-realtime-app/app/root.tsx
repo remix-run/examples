50a3bfa1d5cdd4d3fa439ce3028549b9f353e407
Sergio Xalambrí <hello@sergiodxa.com>
2022-12-16 11:14:19 -0500

    Create /dev/null action for useDataRefresh to work

--- _official-realtime-app/app/root.tsx
+++ _official-realtime-app/app/root.tsx
@@ -50,9 +50,3 @@ function useRealtimeIssuesRevalidation() {
     revalidator.revalidate();
   }, [data, revalidator]);
 }
-
-// FIXME: Pointless action for revalidation until:
-// https://github.com/remix-run/remix/issues/4485
-export async function action() {
-  return json({ ok: true });
-}
