91e297c9bf9174f562aca7861a6ca9c67efeb6d2
sur.la.route <17788706+christopherpickering@users.noreply.github.com>
2023-01-23 14:56:05 -0600

    Update auth-saml-ldap/app/routes/login.tsx
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- auth-saml-ldap/app/routes/login.tsx
+++ auth-saml-ldap/app/routes/login.tsx
@@ -1,6 +1,5 @@
 import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
 import * as React from "react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
