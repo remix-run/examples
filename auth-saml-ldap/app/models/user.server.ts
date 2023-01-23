843192bd396f7cb2c24b3320d366490b40b52181
sur.la.route <17788706+christopherpickering@users.noreply.github.com>
2023-01-23 14:59:04 -0600

    Update auth-saml-ldap/app/models/user.server.ts
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- auth-saml-ldap/app/models/user.server.ts
+++ auth-saml-ldap/app/models/user.server.ts
@@ -2,7 +2,7 @@ import type { User } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
-export type { User, Group } from "@prisma/client";
+export type { User } from "@prisma/client";
 
 export async function getUserById(id: User["id"]) {
   return prisma.user.findUnique({ where: { id }, include: { groups: true } });
