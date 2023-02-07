93e65f61d14bc12cb985e35d5561b97c8c142e33
ikorecky <ikorecky@gmail.com>
2023-02-07 02:57:24 +0200

    fix lint warning

--- mantine/app/entry.server.tsx
+++ mantine/app/entry.server.tsx
@@ -11,7 +11,7 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = renderToString(
+  const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
   responseHeaders.set("Content-Type", "text/html");
