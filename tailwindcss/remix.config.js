1eac42d2021ae36167bffcf40af8488ea6b45982
Michaël De Boey <info@michaeldeboey.be>
2023-02-15 22:22:44 +0100

    feat(tailwindcss): use new `unstable_tailwindcss` flag (#156)

--- tailwindcss/remix.config.js
+++ tailwindcss/remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_tailwind: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,js,jsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
