76c5eb2e18d5de6cbe90c5ec988a5e4c7e5ecb7e
Jan <jan.hesters@rwth-aachen.de>
2023-02-19 13:57:58 +0100

    feat: add AWS SDK v3 upload example

--- file-and-s3-upload-v2/app/utils/s3.server.ts
+++ file-and-s3-upload-v2/app/utils/s3.server.ts
@@ -1,42 +1,30 @@
 import { PassThrough } from "stream";
 
-import type { PutObjectCommandInput } from '@aws-sdk/client-s3';
-import { S3Client } from '@aws-sdk/client-s3';
-import { Upload } from '@aws-sdk/lib-storage';
+import AWS from "aws-sdk";
 import type { UploadHandler } from "@remix-run/node";
 import { writeAsyncIterableToWritable } from "@remix-run/node";
 
-const {
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  S3_STORAGE_BUCKET,
-  S3_STORAGE_REGION,
-} = process.env;
+const { STORAGE_ACCESS_KEY, STORAGE_SECRET, STORAGE_REGION, STORAGE_BUCKET } =
+  process.env;
 
 if (
-  !(
-    AWS_ACCESS_KEY_ID &&
-    AWS_SECRET_ACCESS_KEY &&
-    S3_STORAGE_BUCKET &&
-    S3_STORAGE_REGION
-  )
+  !(STORAGE_ACCESS_KEY && STORAGE_SECRET && STORAGE_REGION && STORAGE_BUCKET)
 ) {
   throw new Error(`Storage is missing required configuration.`);
 }
 
-const uploadStream = ({ Key }: Pick<PutObjectCommandInput, "Key">) => {
-  const client = new S3Client({ region: S3_STORAGE_REGION });
+const uploadStream = ({ Key }: Pick<AWS.S3.Types.PutObjectRequest, "Key">) => {
+  const s3 = new AWS.S3({
+    credentials: {
+      accessKeyId: STORAGE_ACCESS_KEY,
+      secretAccessKey: STORAGE_SECRET,
+    },
+    region: STORAGE_REGION,
+  });
   const pass = new PassThrough();
   return {
     writeStream: pass,
-    promise: new Upload({
-      client,
-      params: {
-        Body: pass,
-        Bucket: S3_STORAGE_BUCKET,
-        Key,
-      },
-    }).done(),
+    promise: s3.upload({ Bucket: STORAGE_BUCKET, Key, Body: pass }).promise(),
   };
 };
 
@@ -46,12 +34,7 @@ export async function uploadStreamToS3(data: any, filename: string) {
   });
   await writeAsyncIterableToWritable(data, stream.writeStream);
   const file = await stream.promise;
-
-  if ('Location' in file) {
-    return file.Location;
-  }
-
-  throw new Error('Upload to S3 aborted');
+  return file.Location;
 }
 
 export const s3UploadHandler: UploadHandler = async ({
