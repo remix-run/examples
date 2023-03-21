ea394283c3d9b9fb6ed3e3529c9d235627753dc0
Daniel Schmitz <danieljfa@gmail.com>
2023-03-21 11:51:53 -0300

    Update picocss/app/root.tsx
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- picocss/app/root.tsx
+++ picocss/app/root.tsx
@@ -15,14 +15,7 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export function links() {
-  return [
-    {
-      rel: "stylesheet",
-      href: styles,
-    },
-  ];
-}
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
