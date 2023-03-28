616bfa28af25049e6903a30264d2256c8f77be26
Michaël De Boey <info@michaeldeboey.be>
2023-03-28 19:45:55 +0200

    chore: update `tailwindcss` version

--- _official-blog-tutorial/tailwind.config.ts
+++ _official-blog-tutorial/tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
