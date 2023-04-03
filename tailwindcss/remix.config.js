0fba8a34c9c3e3df8b2348d896f17e4a3a37d0fb
Michaël De Boey <info@michaeldeboey.be>
2023-04-03 22:02:38 +0200

    feat(tailwindcss): use stable `tailwind` flag

--- tailwindcss/remix.config.js
+++ tailwindcss/remix.config.js
@@ -1,9 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  future: {
-    unstable_tailwind: true,
-  },
+  tailwind: true,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
