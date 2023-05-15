d2169e6b6d8b96958b065d798fc570bcb5235223
Itamar Korecky <ikorecky@gmail.com>
2023-05-15 23:37:04 +0300

    Update mantine/app/entry.client.tsx
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- mantine/app/entry.client.tsx
+++ mantine/app/entry.client.tsx
@@ -12,11 +12,3 @@ startTransition(() => {
       </ClientProvider>
     </StrictMode>
   );
-
-if (typeof requestIdleCallback === "function") {
-  requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  setTimeout(hydrate, 1);
-}
