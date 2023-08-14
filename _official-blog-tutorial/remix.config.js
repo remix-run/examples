d9e8185a55acc90bc97e68e14231bb350d26dc80
Michaël De Boey <info@michaeldeboey.be>
2023-08-14 20:26:38 +0200

    feat(_official-blog-tutorial): update to latest `indie-stack`

--- _official-blog-tutorial/remix.config.js
+++ _official-blog-tutorial/remix.config.js
@@ -2,12 +2,15 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
   ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
   postcss: true,
+  serverModuleFormat: "cjs",
   tailwind: true,
 };
