1dae4bedc181ade3ff775a5977b9c910a1c3ca51
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-25 15:13:39 -0600

    add contact tutorial

---
+++ _official-contacts-tutorial/src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+	return (
+		<p id="zero-state">
+			This is a demo for React Router.
+			<br />
+			Check out{' '}
+			<a href="https://reactrouter.com">the docs at reactrouter.com</a>.
+		</p>
+	)
+}
