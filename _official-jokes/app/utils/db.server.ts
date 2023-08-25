371501ea2446c5a1963019d42e8f43cda0b2e26f
Michaël De Boey <info@michaeldeboey.be>
2023-08-26 00:59:41 +0200

    chore(_official-jokes-tutorial): remove explicit DB connection

--- _official-jokes/app/utils/db.server.ts
+++ _official-jokes/app/utils/db.server.ts
@@ -4,6 +4,5 @@ import { singleton } from "./singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 const db = singleton("prisma", () => new PrismaClient());
-db.$connect();
 
 export { db };
