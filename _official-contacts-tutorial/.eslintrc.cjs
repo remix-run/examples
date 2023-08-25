c22fdd1252c270b28fcc0a8e6d03e22cea940ea2
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-25 15:18:15 -0600

    remove unused plugin

--- _official-contacts-tutorial/.eslintrc.cjs
+++ _official-contacts-tutorial/.eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
   rules: {
     "react/prop-types": "off",
   },
