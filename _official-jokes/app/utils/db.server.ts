4eace410e3ddbe37b7ef9ac91463fea5bccda606
Michaël De Boey <info@michaeldeboey.be>
2023-08-26 00:56:02 +0200

    feat(_official-jokes-tutorial): update to be in line with latest template (#319)

--- _official-jokes/app/utils/db.server.ts
+++ _official-jokes/app/utils/db.server.ts
@@ -1,23 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let db: PrismaClient;
+import { singleton } from "./singleton.server";
 
-declare global {
-  var __db__: PrismaClient | undefined;
-}
-
-// This is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-// In production, we'll have a single connection to the DB.
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
-  if (!global.__db__) {
-    global.__db__ = new PrismaClient();
-  }
-  db = global.__db__;
-  db.$connect();
-}
+// Hard-code a unique key, so we can look up the client when this module gets re-imported
+const db = singleton("prisma", () => new PrismaClient());
+db.$connect();
 
 export { db };
