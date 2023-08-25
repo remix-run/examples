4fa485e8cd8ec31612890b47df8c63fa77a167cf
Michaël De Boey <info@michaeldeboey.be>
2023-08-26 01:00:37 +0200

    chore(_official-jokes-tutorial): export db in one go

--- _official-jokes/app/utils/db.server.ts
+++ _official-jokes/app/utils/db.server.ts
@@ -3,6 +3,4 @@ import { PrismaClient } from "@prisma/client";
 import { singleton } from "./singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
-const db = singleton("prisma", () => new PrismaClient());
-
-export { db };
+export const db = singleton("prisma", () => new PrismaClient());
