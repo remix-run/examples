1b6a8631afb5d3ad2b03f8d32f876326d8544dad
Kent C. Dodds <me+github@kentcdodds.com>
2023-08-25 15:16:16 -0600

    format and yarn

--- _official-contacts-tutorial/src/main.jsx
+++ _official-contacts-tutorial/src/main.jsx
@@ -1,53 +1,56 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom'
-import ErrorPage from './error-page'
-import './index.css'
-import Root, { action as rootAction, loader as rootLoader } from './routes/root'
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./error-page";
+import "./index.css";
+import Root, {
+  action as rootAction,
+  loader as rootLoader,
+} from "./routes/root";
 import Contact, {
-	action as contactAction,
-	loader as contactLoader,
-} from './routes/contact'
-import EditContact, { action as editAction } from './routes/edit'
-import { action as destroyAction } from './routes/destroy'
-import Index from './routes'
+  action as contactAction,
+  loader as contactLoader,
+} from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route
-			path="/"
-			element={<Root />}
-			loader={rootLoader}
-			action={rootAction}
-			errorElement={<ErrorPage />}
-		>
-			<Route errorElement={<ErrorPage />}>
-				<Route index element={<Index />} />
-				<Route
-					path="contacts/:contactId"
-					element={<Contact />}
-					loader={contactLoader}
-					action={contactAction}
-				/>
-				<Route
-					path="contacts/:contactId/edit"
-					element={<EditContact />}
-					loader={contactLoader}
-					action={editAction}
-				/>
-				<Route path="contacts/:contactId/destroy" action={destroyAction} />
-			</Route>
-		</Route>,
-	),
-)
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>,
+  ),
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
--- _official-contacts-tutorial/src/routes/destroy.jsx
+++ _official-contacts-tutorial/src/routes/destroy.jsx
@@ -1,7 +1,7 @@
-import { redirect } from 'react-router-dom'
-import { deleteContact } from '../contacts'
+import { redirect } from "react-router-dom";
+import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-	await deleteContact(params.contactId)
-	return redirect('/')
+  await deleteContact(params.contactId);
+  return redirect("/");
 }
