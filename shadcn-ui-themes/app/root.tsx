c24ef594a287919c056da5a9518904d0627888e8
Tobias Geiselmann <11350542+TobiasGeiselmann@users.noreply.github.com>
2023-11-17 15:15:06 +0100

    Add example for shadcn/ui with remix-themes

---
+++ shadcn-ui-themes/app/root.tsx
@@ -0,0 +1,62 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from "@remix-run/react";
+import { ThemeProvider, useTheme } from "remix-themes";
+import stylesheet from "~/tailwind.css";
+import { themeSessionResolver } from "./sessions.server";
+
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+
+// Wrap the app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  )
+}
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
+];
+
+function App() {
+  const [theme] = useTheme()
+  
+  return (
+    // Conditionally add the `dark` class to the html element.
+    // See https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
+    <html lang="en" className={theme === "dark" ? "dark": ""}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
