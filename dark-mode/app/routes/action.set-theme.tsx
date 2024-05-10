6e2d08a37f09b539f6012cb8c2626ed9bc8bb122
bravo-kernel <bravo-kernel@users.noreply.github.com>
2024-05-10 07:41:04 +0200

    Update action.set-theme.tsx
    Replace deprecated `ActionArgs` with `ActionFunctionArgs`

--- dark-mode/app/routes/action.set-theme.tsx
+++ dark-mode/app/routes/action.set-theme.tsx
@@ -1,10 +1,10 @@
-import type { ActionArgs } from "@remix-run/node";
+import type { ActionFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 
 import { isTheme } from "~/utils/theme-provider";
 import { getThemeSession } from "~/utils/theme.server";
 
-export const action = async ({ request }: ActionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   const themeSession = await getThemeSession(request);
   const requestText = await request.text();
   const form = new URLSearchParams(requestText);
