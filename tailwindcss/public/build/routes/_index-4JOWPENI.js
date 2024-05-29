11c626321808eee8c45de2f7a7a8e5ecbca699fc
Mehdi Achour <machour@gmail.com>
2024-05-29 15:12:10 +0100

    refactor(tailwindcss): Switch to V2 + Vite

---
+++ tailwindcss/public/build/routes/_index-4JOWPENI.js
@@ -0,0 +1,37 @@
+import {
+  __toESM,
+  init_buffer,
+  init_dirname,
+  init_filename,
+  init_global,
+  init_process,
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-3IUBNEQW.js";
+
+// browser-route-module:routes/_index.tsx?browser
+init_global();
+init_dirname();
+init_filename();
+init_buffer();
+init_process();
+
+// app/routes/_index.tsx
+init_global();
+init_dirname();
+init_filename();
+init_buffer();
+init_process();
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+var meta = () => [{ title: "New Remix App" }];
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { className: "text-6xl font-bold text-red-700", children: "Hello World!" }, void 0, false, {
+    fileName: "app/routes/_index.tsx",
+    lineNumber: 6,
+    columnNumber: 10
+  }, this);
+}
+export {
+  Index as default,
+  meta
+};
+//# sourceMappingURL=/build/routes/_index-4JOWPENI.js.map
