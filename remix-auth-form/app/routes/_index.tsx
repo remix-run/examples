7c087e40faf76a5398b019e729681d20a90cf21b
Shane Walker <shane@swalker.dev>
2024-05-31 05:22:11 -0500

    refactor(remix-auth-form): Switch to V2 + Vite (#364)
    Co-authored-by: Mehdi Achour <machour@gmail.com>

---
+++ remix-auth-form/app/routes/_index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "@remix-run/react";
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
--- remix-auth-form/app/routes/login.tsx
+++ remix-auth-form/app/routes/login.tsx
@@ -25,29 +25,32 @@ export default function Screen() {
   const { error } = useLoaderData<typeof loader>();
 
   return (
-    <Form method="post">
-      {error ? <div>{error.message}</div> : null}
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          defaultValue="user@domain.tld"
-        />
-      </div>
+    <>
+      <pre>/login</pre>
+      <Form method="post">
+        {error ? <div>{error.message}</div> : null}
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            defaultValue="user@domain.tld"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          defaultValue="test"
-        />
-      </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            defaultValue="test"
+          />
+        </div>
 
-      <button>Log In</button>
-    </Form>
+        <button>Log In</button>
+      </Form>
+    </>
   );
 }
