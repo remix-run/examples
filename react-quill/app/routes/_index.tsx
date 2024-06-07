4dea3c4997f346d67355ae18922eaf76d20965dd
Mehdi Achour <machour@gmail.com>
2024-06-07 16:15:23 +0100

    refactor(react-quill): Switch to V2 + Vite (#506)

--- react-quill/app/routes/_index.tsx
+++ react-quill/app/routes/_index.tsx
@@ -1,9 +1,9 @@
 import { json } from "@remix-run/node";
-import type { ActionArgs, LinksFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import type { ActionFunctionArgs, LinksFunction } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 import { useState } from "react";
-import stylesheetQuill from "react-quill/dist/quill.snow.css";
-import { ClientOnly } from "remix-utils";
+import stylesheetQuill from "react-quill/dist/quill.snow.css?url";
+import { ClientOnly } from "remix-utils/client-only";
 
 import { FallbackComponent } from "~/components/fallback-component";
 import { TextEditor } from "~/components/textEditor.client";
@@ -12,19 +12,22 @@ export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheetQuill },
 ];
 
-export const action = async ({ request }: ActionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   const form = await request.formData();
   const textEditorValue = form.get("textEditor");
   return json({ textEditorValue });
 };
 
 export default function Index() {
+  const data = useActionData();
   const [textEditor, setTextEditor] = useState("");
   return (
     <Form method="post">
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : null}
       <ClientOnly fallback={<FallbackComponent />}>
         {() => (
           <TextEditor
+            name="editor"
             theme="snow"
             placeholder="Write description"
             onChange={setTextEditor}
