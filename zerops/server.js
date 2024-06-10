34c484e6ed04c48b5fd2a5dbf40b253df3261b54
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-10 19:10:26 +0530

    feat: installGlobals in server.js

--- zerops/server.js
+++ zerops/server.js
@@ -1,7 +1,10 @@
 import { createRequestHandler } from "@remix-run/express";
+import { installGlobals } from "@remix-run/node";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+
+installGlobals();
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -9,7 +12,7 @@ const viteDevServer =
     : await import("vite").then((vite) =>
         vite.createServer({
           server: { middlewareMode: true },
-        })
+        }),
       );
 
 const remixHandler = createRequestHandler({
@@ -32,7 +35,7 @@ if (viteDevServer) {
   // Vite fingerprints its assets so we can cache forever.
   app.use(
     "/assets",
-    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+    express.static("build/client/assets", { immutable: true, maxAge: "1y" }),
   );
 }
 
@@ -47,5 +50,5 @@ app.all("*", remixHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
-  console.log(`Express server listening at http://localhost:${port}`)
+  console.log(`Express server listening at http://localhost:${port}`),
 );
