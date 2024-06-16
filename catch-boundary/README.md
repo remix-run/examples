1e8113da23979e99928c6bed99bc2d423b2fd3c1
Mehdi Achour <machour@gmail.com>
2024-06-16 17:48:47 +0100

    refactor(catch-boundary): Switch to V2 + Vite

--- catch-boundary/README.md
+++ catch-boundary/README.md
@@ -1,19 +1,19 @@
-# CatchBoundary Example
+# ErrorBoundary Example
 
-If you want to handle _expected_ errors, you use a `CatchBoundary` to catch those types of errors. Think about HTTP-400-level errors like unauthorized etc.
+If you want to handle errors, export an `ErrorBoundary` from your route.
 
 ## Preview
 
 Open this example on [CodeSandbox](https://codesandbox.com):
 
-[![Open in CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/remix-run/examples/tree/main/catch-boundary)
+[![Open in CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/remix-run/examples/tree/main/error-boundary)
 
 ## Example
 
-In this example, we have a list of users and one user that does not exist. When you navigate to the user that does not exist, our CatchBoundary renders in place of the component for that route.
+In this example, we have a list of users and one user that does not exist. When you navigate to the user that does not exist, our ErrorBoundary renders in place of the component for that route.
 
-Check [app/routes/users/$userId.tsx](app/routes/users/$userId.tsx) to see the CatchBoundary in action.
+Check [app/routes/users/$userId.tsx](app/routes/users/$userId.tsx) to see the ErrorBoundary in action.
 
 ## Related Links
 
-- [CatchBoundary in the Remix Docs](https://remix.run/route/catch-boundary)
+- [ErrorBoundary in the Remix Docs](https://remix.run/route/error-boundary)
--- catch-boundary/app/routes/users.$userId.tsx
+++ catch-boundary/app/routes/users.$userId.tsx
@@ -1,19 +1,23 @@
-import type { LoaderArgs, MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useCatch, useLoaderData, useParams } from "@remix-run/react";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useParams,
+  useRouteError,
+} from "@remix-run/react";
 
 import { getUsers } from "~/data.server";
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
-  // When the response is thrown for a missing user, the data will be the
-  // thrown response.
+  // Handle our 404 gracefully by setting a generic error as page title
   if (!data || !data.user) {
-    return { title: "User not found!" };
+    return [{ title: "User not found!" }];
   }
-  return { title: data.user.name };
+  return [{ title: data.user.name }];
 };
 
-export const loader = async ({ params }: LoaderArgs) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
   const userId = params.userId;
 
   const users = getUsers();
@@ -32,28 +36,22 @@ export default function User() {
   return <div>Hi there {user.name}!</div>;
 }
 
-// Export a CatchBoundary and use the useCatch hook to handle thrown responses
-// like the 404 we have in our loader.
+// Export an ErrorBoundary and use the useRouteError/isRouteErrorResponse
+// combo to handle thrown responses like the 404 we have in our loader.
 // You can also catch thrown responses from actions as well.
-export function CatchBoundary() {
-  const caught = useCatch();
-  const params = useParams();
+export function ErrorBoundary() {
+  const error = useRouteError();
 
-  switch (caught.status) {
-    case 404: {
-      return <h2>User with ID "{params.userId}" not found!</h2>;
-    }
-    default: {
-      // if we don't handle this then all bets are off. Just throw an error
-      // and let the nearest ErrorBoundary handle this
-      throw new Error(`${caught.status} not handled`);
-    }
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    const params = useParams();
+
+    return (
+      <span style={{ color: "red" }}>
+        User with ID "{params.userId}" not found!
+      </span>
+    );
   }
-}
 
-// this will handle unexpected errors (like the default case above where the
-// CatchBoundary gets a response it's not prepared to handle).
-export function ErrorBoundary({ error }: { error: Error }) {
   console.error(error);
 
   return (
