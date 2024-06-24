f494d58abf04fa1a598029442460bd874518e788
anzweidrej <123416792+anzweidrej@users.noreply.github.com>
2024-06-24 21:29:10 +0200

    fix: update product grid

--- medusa-js/app/routes/products._index.tsx
+++ medusa-js/app/routes/products._index.tsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 
 import { createClient } from "~/lib/client";
 
@@ -13,14 +13,24 @@ export default function Products() {
   const products = useLoaderData<typeof loader>();
 
   return (
-    <div className="w-full p-4 my-8">
-      <h1 className="text-center">Latest Arrivals</h1>
+    <div className="font-sans p-4">
+      <h1 className="text-3xl text-center">Latest Arrivals</h1>
       {products && !products.length && <span>No products found!</span>}
       {products && products.length > 0 && (
-        <ul>
+        <ul className="grid grid-cols-1 gap-6 px-4 mt-8 md:px-12 lg:px-6 xl:px-4 xl:gap-6 2xl:px-24 2xl:gap-6 justify-items-center md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {products.map((product) => (
             <li key={product.id}>
-              <Link to={`/products/${product.id}`}>{product.title}</Link>
+              <a
+                className="text-blue-700 underline visited:text-purple-900"
+                href={`/products/${product.id}`}
+              >
+                <img
+                  className="w-80"
+                  src={product.thumbnail as string}
+                  alt={product.title}
+                />
+                {product.title}
+              </a>
             </li>
           ))}
         </ul>
