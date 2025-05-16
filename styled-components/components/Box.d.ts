c68902a0a339fcc680320f5551de49e138d1f12d
wetteyve <yveswetter@gmx.ch>
2025-05-16 12:29:01 +0200

    refactor: remove falsly commited declaration file

--- styled-components/components/Box.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const Box: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement>, never>> & string;
